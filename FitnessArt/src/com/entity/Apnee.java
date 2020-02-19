a811074c05149f14a759e0b80a6e617fb157ae29
lione <lione@DESKTOP-FVA4L9J>
2020-02-19 19:42:09 +0100

    Modification Apnee

--- FitnessArt/src/com/entity/Apnee.java
+++ FitnessArt/src/com/entity/Apnee.java
@@ -8,55 +8,17 @@ import javax.persistence.Id;
 
 
 @Entity
-public class Apnee implements Serializable {
+public class Apnee extends Activity implements Serializable {
 	
-	@Id
-	@GeneratedValue
-	private int idApnee;
-	public int getIdApnee() {
-		return idApnee;
-	}
-
-
-
-	public void setIdApnee(int idApnee) {
-		this.idApnee = idApnee;
-	}
-
 	private int depth;
 	private int time;
-
 	
 	public Apnee() {
-		
+		super();
 	}
 	
-	
-
-	public int getTime() {
-		return time;
+	public Apnee(int idActivity, String nameActivity) {
+		super(idActivity, nameActivity);
 	}
-
-
-
-	public void setTime(int time) {
-		this.time = time;
-	}
-
-
-
-	public int getDepth() {
-		return depth;
-	}
-
-	public void setDepth(int depth) {
-		this.depth = depth;
-	}
-
 	
-
-	
-	
-	
-
 }
