b9bb488b6bda92595bbee677257c3efec9250857
lione <lione@DESKTOP-FVA4L9J>
2020-02-19 19:42:45 +0100

    Modification Jogging

--- FitnessArt/src/com/entity/Jogging.java
+++ FitnessArt/src/com/entity/Jogging.java
@@ -8,46 +8,17 @@ import javax.persistence.Id;
 
 
 @Entity
-public class Jogging implements Serializable {
+public class Jogging extends Activity implements Serializable {
 	
-	public int getIdJogging() {
-		return idJogging;
-	}
-
-	public void setIdJogging(int idJogging) {
-		this.idJogging = idJogging;
-	}
-
-	@Id
-	@GeneratedValue
-	private int idJogging;
-	private float distance;
+	private int distance;
 	private int time;
 	
 	public Jogging() {
-		
+		super();
 	}
-
-	public int getTime() {
-		return time;
-	}
-
-	public void setTime(int time) {
-		this.time = time;
-	}
-
-	public float getDistance() {
-		return distance;
-	}
-
-	public void setDistance(float distance) {
-		this.distance = distance;
-	}
-
-
-
-
 	
+	public Jogging(int idActivity, String nameActivity) {
+		super(idActivity, nameActivity);
+	}
 	
-
 }
