601d3f158da205a3d4f3f4adaad74175a190e89d
lione <lione@DESKTOP-FVA4L9J>
2020-02-19 19:43:43 +0100

    Modification TirArc

--- FitnessArt/src/com/entity/TirArc.java
+++ FitnessArt/src/com/entity/TirArc.java
@@ -7,48 +7,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class TirArc implements Serializable  {
+public class TirArc extends Activity implements Serializable  {
 	
-	@Id
-	@GeneratedValue
-	private int idTirArc;
 	private int nbrPoint;
 	private int distance;
 	
 	public TirArc() {
-	}
-
-	public TirArc(int idTirArc, int nbrPoint, int distance) {
-		this.idTirArc = idTirArc;
-		this.nbrPoint = nbrPoint;
-		this.distance = distance;
-	}
-
-	public int getIdTirArc() {
-		return idTirArc;
-	}
-
-	public void setIdTirArc(int idTirArc) {
-		this.idTirArc = idTirArc;
-	}
-
-	public int getNbrPoint() {
-		return nbrPoint;
-	}
-
-	public void setNbrPoint(int nbrPoint) {
-		this.nbrPoint = nbrPoint;
-	}
-
-	public int getDistance() {
-		return distance;
-	}
-
-	public void setDistance(int distance) {
-		this.distance = distance;
+		super();
 	}
 	
+	public TirArc(int idActivity, String nameActivity) {
+		super(idActivity, nameActivity);
+	}
 	
-	
-
 }
