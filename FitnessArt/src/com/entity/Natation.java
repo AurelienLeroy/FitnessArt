7e05c4dd7e109b2b9e9815fd54d5881c36fd4f61
lione <lione@DESKTOP-FVA4L9J>
2020-02-19 19:43:22 +0100

    Modification Natation

--- FitnessArt/src/com/entity/Natation.java
+++ FitnessArt/src/com/entity/Natation.java
@@ -7,46 +7,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Natation implements Serializable {
+public class Natation extends Activity implements Serializable {
 	
-	@Id
-	@GeneratedValue
-	private int idNatation;
 	private String type;
 	private int distance;
 	
 	public Natation() {
-		
-	}
-
-	public int getIdNatation() {
-		return idNatation;
-	}
-
-	public void setIdNatation(int idNatation) {
-		this.idNatation = idNatation;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-
-
-
-
-	public int getDistance() {
-		return distance;
-	}
-
-	public void setDistance(int distance) {
-		this.distance = distance;
+		super();
 	}
 	
+	public Natation(int idActivity, String nameActivity) {
+		super(idActivity, nameActivity);
+	}
 	
-
 }
